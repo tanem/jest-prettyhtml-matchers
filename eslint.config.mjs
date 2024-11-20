03b986acf3810778157d6874cae75adf35f646cc
Tane Morgan <464864+tanem@users.noreply.github.com>
2024-11-21 06:08:54 +1300

    Migrate to eslint flat file config

---
+++ eslint.config.mjs
@@ -0,0 +1,51 @@
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default [{
+    ignores: [
+        "**/dist/",
+        "**/node_modules/",
+        "**/coverage/",
+        "**/jest.config.*",
+        "test/**/src/",
+    ],
+}, ...compat.extends("plugin:@typescript-eslint/recommended", "prettier"), {
+    plugins: {
+        "@typescript-eslint": typescriptEslint,
+    },
+
+    languageOptions: {
+        parser: tsParser,
+        ecmaVersion: 5,
+        sourceType: "module",
+
+        parserOptions: {
+            project: "tsconfig.json",
+        },
+    },
+
+    rules: {
+        "@typescript-eslint/ban-ts-comment": "off",
+        "@typescript-eslint/explicit-module-boundary-types": "off",
+        "@typescript-eslint/no-explicit-any": "off",
+        "@typescript-eslint/no-namespace": "off",
+    },
+}, {
+    files: ["**/*.js"],
+
+    rules: {
+        "@typescript-eslint/no-var-requires": "off",
+    },
+}];
