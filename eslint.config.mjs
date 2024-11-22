cc08c9c3dfb4c3cf37f4b2d43b1a3fddb8dbd7b1
Tane Morgan <464864+tanem@users.noreply.github.com>
2024-11-23 06:34:03 +1300

    Update @typescript-eslint deps

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,51 +1,56 @@
-import typescriptEslint from "@typescript-eslint/eslint-plugin";
-import tsParser from "@typescript-eslint/parser";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+import typescriptEslint from '@typescript-eslint/eslint-plugin'
+import tsParser from '@typescript-eslint/parser'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+import js from '@eslint/js'
+import { FlatCompat } from '@eslint/eslintrc'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
-});
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+})
 
-export default [{
+export default [
+  {
     ignores: [
-        "**/dist/",
-        "**/node_modules/",
-        "**/coverage/",
-        "**/jest.config.*",
-        "test/**/src/",
+      '**/dist/',
+      '**/node_modules/',
+      '**/coverage/',
+      '**/jest.config.*',
+      'test/**/src/',
     ],
-}, ...compat.extends("plugin:@typescript-eslint/recommended", "prettier"), {
+  },
+  ...compat.extends('plugin:@typescript-eslint/recommended', 'prettier'),
+  {
     plugins: {
-        "@typescript-eslint": typescriptEslint,
+      '@typescript-eslint': typescriptEslint,
     },
 
     languageOptions: {
-        parser: tsParser,
-        ecmaVersion: 5,
-        sourceType: "module",
+      parser: tsParser,
+      ecmaVersion: 5,
+      sourceType: 'module',
 
-        parserOptions: {
-            project: "tsconfig.json",
-        },
+      parserOptions: {
+        project: 'tsconfig.json',
+      },
     },
 
     rules: {
-        "@typescript-eslint/ban-ts-comment": "off",
-        "@typescript-eslint/explicit-module-boundary-types": "off",
-        "@typescript-eslint/no-explicit-any": "off",
-        "@typescript-eslint/no-namespace": "off",
+      '@typescript-eslint/ban-ts-comment': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-namespace': 'off',
     },
-}, {
-    files: ["**/*.js"],
+  },
+  {
+    files: ['**/*.js'],
 
     rules: {
-        "@typescript-eslint/no-var-requires": "off",
+      '@typescript-eslint/no-var-requires': 'off',
     },
-}];
+  },
+]
